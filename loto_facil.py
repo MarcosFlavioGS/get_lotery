1a0ebbf4d61fee78fe384cd4615e77561e4337e3
Marcos Flavio <mfghost69@gmail.com>
2023-01-06 21:01:45 -0300

    Atualizando os números

--- loto_facil.py
+++ loto_facil.py
@@ -1,11 +1,5 @@
 from aux_func import generator, line_occurrence
 
-def check_for_occurrence(occurrence, result):
-    for i in occurrence:
-        if i == result:
-            return True
-    return False
-
 def loto_facil():
     numbers = []
     for i in range(1, 26):
@@ -15,18 +9,13 @@ def loto_facil():
 
     for item in occurrence:
         if occurrence[item] > 1:
-            print(f"Volante {item} já saiu {occurrence[item]} vezes")
+            print(f"\nVolante {item} já saiu {occurrence[item]} vezes nos ultimos {len(occurrence)} sorteios")
 
     while (True):
         choice = int(input("\nGostaria de quantos números ? (15 - 20): "))
         result = generator(numbers, choice)
         print(f"\nNúmeros sorteados: {result}")
 
-        if check_for_occurrence(occurrence, result) == True:
-            print(f"\nEsse volante já saiu {occurrence[result]} vezes")
-        else:
-            print(f"\nEsse volante ainda não saiu nos ultimos {len(occurrence)} sorteios")
-        
         re = input("\nDeseja gerar mais números ? (1 - Sim / 2 - Não) ")
         if re == "1":
             continue
