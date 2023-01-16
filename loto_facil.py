c0d550ec5af051445591315d0034132da925c472
Marcos Flavio <mfghost69@gmail.com>
2023-01-16 20:18:15 -0300

    Atualizando arquivos de texto

--- loto_facil.py
+++ loto_facil.py
@@ -1,5 +1,17 @@
 from aux_func import generator, line_occurrence
 
+def check_occurrence(dict, list, str):
+    for key in dict:
+        count = 0
+        key_elements = key.split(' ')
+        for element in key_elements:
+            if element in list:
+                count += 1
+            if count == 15:
+                str = key
+                return True
+    return False
+
 def loto_facil():
     numbers = []
     for i in range(1, 26):
@@ -15,6 +27,12 @@ def loto_facil():
         choice = int(input("\nGostaria de quantos números ? (15 - 20): "))
         result = generator(numbers, choice)
         print(f"\nNúmeros sorteados: {result}")
+
+        str = ""
+        if check_occurrence(occurrence, numbers, str) == True:
+            print(f"\nEsses números já saíram {occurrence[str]} vezes nos últimos {len(occurrence)} sorteios")
+        else:
+            print("\nEsses números ainda não saíram nos ultimos sorteios")
 
         re = input("\nDeseja gerar mais números ? (1 - Sim / 2 - Não) ")
         if re == "1":
